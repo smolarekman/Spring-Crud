e3aef15fb1fd3fe871d1d999c113d4665749e797
smolarekman <smolarekman@gmail.com>
2019-04-01 12:57:24 +0200

    first model

---
+++ src/main/java/com/example/simplecrud/model/Person.java
@@ -0,0 +1,53 @@
+package com.example.simplecrud.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "people")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first-name")
+    private String firstName;
+
+    @Column(name = "last-name")
+    private String lastName;
+
+    @Column(name = "phone-number")
+    private int phoneNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
